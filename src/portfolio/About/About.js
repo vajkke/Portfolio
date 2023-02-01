06914689cd4d53b70aded0060bdcd9bd2c4dc445
Aleksandar V <95486215+vajkke@users.noreply.github.com>
2023-02-01 21:45:29 +0100

    updated about

--- src/portfolio/About/About.js
+++ src/portfolio/About/About.js
@@ -7,26 +7,26 @@ const About = () => {
         <h1 className="who--heading">Who am I?</h1>
         <div className="who--line"></div>
         <div className="who--text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel
-          mauris vestibulum, lobortis mi eget, vestibulum tortor. Vestibulum
-          quis nunc vulputate diam malesuada auctor. Donec consectetur mattis
-          dapibus. Vivamus nulla augue, pulvinar sed purus in, iaculis tincidunt
-          metus. Suspendisse potenti. Nam consequat nunc neque, at consequat sem
-          ultricies eu. Sed eget finibus tellus. Donec imperdiet orci sed ligula
-          pharetra, eu.
+          I am a 23 year old Front End developer from Serbia. My love for
+          technology and desire to learn have driven me to self-teach web
+          development for the past year, with a focus on Front End. However, my
+          ultimate goal is to expand my expertise and become a Full Stack Web
+          developer, capable of tackling complex projects and delivering high
+          quality results.
         </div>
       </div>
       <div className="story--container about--container">
         <h1 className="story--heading">What's my story?</h1>
         <div className="story--line"></div>
         <div className="story--text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel
-          mauris vestibulum, lobortis mi eget, vestibulum tortor. Vestibulum
-          quis nunc vulputate diam malesuada auctor. Donec consectetur mattis
-          dapibus. Vivamus nulla augue, pulvinar sed purus in, iaculis tincidunt
-          metus. Suspendisse potenti. Nam consequat nunc neque, at consequat sem
-          ultricies eu. Sed eget finibus tellus. Donec imperdiet orci sed ligula
-          pharetra, eu.
+          My initial goal was to pursue a career as a freelance web developer,
+          but I decided to take an internship opportunity at a startup company
+          instead. During my time there, I gained valuable experience
+          participating in client business calls and contributing to the
+          development of the company's website. This experience has further
+          solidified my passion for web development and has given me a greater
+          appreciation for the technical and interpersonal skills required to
+          succeed in this field.
         </div>
       </div>
     </section>
